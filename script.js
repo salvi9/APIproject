f7e309e5fa08417a41a1c6d276fded7ee769fca5
salvi9 <dxherny@gmail.com>
2024-07-13 17:22:27 -0600

    update for project

---
+++ script.js
@@ -0,0 +1,60 @@
+const apiKey = "e17d8736d2530500fa57de9f4e20a015";
+const appId = "68110ced";
+const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${appId}&app_key=${apiKey}`;
+const recipeContainer = document.querySelector("#recipe-container");
+const userSearch = document.querySelector("#userSearch");
+const btnSearch = document.querySelector(".searchButton");
+
+btnSearch.addEventListener("click", (e) => {
+  const userValue = userSearch.value;
+  renderRecipe(userValue);
+});
+
+function renderRecipe(type = "beef") {
+  const url = baseUrl + `&q=${type}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      loadRecipes(data.hits);
+    })
+    .catch((error) => console.log(error));
+}
+
+renderRecipe();
+
+const loadRecipes = (recipeList = []) => {
+  recipeContainer.innerHTML = "";
+  recipeList.forEach((recipeObj) => {
+    const {
+      label: recipeTitle,
+      ingredientLines,
+      image: recipeImage,
+    } = recipeObj.recipe;
+    const htmlStr = `
+        <div class="recipe">
+            <div class="title-container">
+                <a href="#">
+                <div class="recipe-img">
+                    <img src=${recipeImage}>
+                </div>
+                <div class="recipe-title"><h4>${recipeTitle}</h4></div>
+                </a>
+            </div>
+        </div>
+        `;
+    recipeContainer.insertAdjacentHTML("beforeend", htmlStr);
+  });
+};
+
+{
+  /* <div class="recipe-text">
+                <ul>
+                    ${ingredientLines
+                      .map(
+                        (ingredient) =>
+                          `<li  class="list-style">${ingredient}</li>`
+                      )
+                      .join("")}
+                </ul>
+            </div> */
+}
